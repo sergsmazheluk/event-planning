bd74a4e0eff89278db969a8693e16f172ce20a4c
smazhserg@gmail.com <smazhserg@gmail.com>
2025-10-24 20:06:49 +0300

    "feat(app): AutoMapper profile for domain to DTO"

---
+++ src/EventPlanning.Application/Mappings/EventProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EventPlanning.Domain.Events;
+
+namespace EventPlanning.Application.Mappings
+{
+    public class EventProfile : Profile
+    {
+        public EventProfile()
+        {
+            //CreateMap<FieldDefinition, FieldDefinitionDto>();
+            //CreateMap<EventDefinition, EventDefinitionDto>();
+            //CreateMap<Event, EventDto>();
+        }
+    }
+}
